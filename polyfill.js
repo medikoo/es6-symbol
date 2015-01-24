c1a5b3c98ded8fa87de7c2505d468ff3d39b4bc4
Josh Perez <josh@goatslacker.com>
2015-01-24 14:22:29 -0800

    Removes dependency on d

--- polyfill.js
+++ polyfill.js
@@ -1,9 +1,17 @@
 'use strict';
 
-var d = require('d')
+var create = Object.create, defineProperties = Object.defineProperties
+  , d, generateName, Symbol;
 
-  , create = Object.create, defineProperties = Object.defineProperties
-  , generateName, Symbol;
+d = function (value, o) {
+  o = o || {};
+  return {
+    value: value,
+    configurable: o.c || false,
+    writable: o.w || false,
+    enumerable: o.e || false
+  };
+};
 
 generateName = (function () {
 	var created = create(null);
@@ -24,30 +32,30 @@ module.exports = Symbol = function (description) {
 	symbol = create(Symbol.prototype);
 	description = (description === undefined ? '' : String(description));
 	return defineProperties(symbol, {
-		__description__: d('', description),
-		__name__: d('', generateName(description))
+		__description__: d(description),
+		__name__: d(generateName(description))
 	});
 };
 
 Object.defineProperties(Symbol, {
-	create: d('', Symbol('create')),
-	hasInstance: d('', Symbol('hasInstance')),
-	isConcatSpreadable: d('', Symbol('isConcatSpreadable')),
-	isRegExp: d('', Symbol('isRegExp')),
-	iterator: d('', Symbol('iterator')),
-	toPrimitive: d('', Symbol('toPrimitive')),
-	toStringTag: d('', Symbol('toStringTag')),
-	unscopables: d('', Symbol('unscopables'))
+	create: d(Symbol('create')),
+	hasInstance: d(Symbol('hasInstance')),
+	isConcatSpreadable: d(Symbol('isConcatSpreadable')),
+	isRegExp: d(Symbol('isRegExp')),
+	iterator: d(Symbol('iterator')),
+	toPrimitive: d(Symbol('toPrimitive')),
+	toStringTag: d(Symbol('toStringTag')),
+	unscopables: d(Symbol('unscopables'))
 });
 
 defineProperties(Symbol.prototype, {
 	properToString: d(function () {
 		return 'Symbol (' + this.__description__ + ')';
 	}),
-	toString: d('', function () { return this.__name__; })
+	toString: d(function () { return this.__name__; })
 });
-Object.defineProperty(Symbol.prototype, Symbol.toPrimitive, d('',
+Object.defineProperty(Symbol.prototype, Symbol.toPrimitive, d(
 	function (hint) {
 		throw new TypeError("Conversion of symbol objects is not allowed");
 	}));
-Object.defineProperty(Symbol.prototype, Symbol.toStringTag, d('c', 'Symbol'));
+Object.defineProperty(Symbol.prototype, Symbol.toStringTag, d('Symbol', { c: true }));
