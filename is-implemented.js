78b8be8bbfc4ba849738dbe492db5b0133c5ee20
Mariusz Nowak <medyk@medikoo.com>
2016-05-17 12:38:20 +0200

    Fix isImplemented logic

--- is-implemented.js
+++ is-implemented.js
@@ -7,18 +7,11 @@ module.exports = function () {
 	if (typeof Symbol !== 'function') return false;
 	symbol = Symbol('test symbol');
 	try { String(symbol); } catch (e) { return false; }
-	if ((typeof Symbol.iterator === 'symbol') &&
-			(typeof Symbol.toPrimitive === 'symbol') &&
-			(typeof Symbol.toStringTag === 'symbol')) {
-		return true;
-	}
 
-	// Return 'true' for polyfills
-	if (!validTypes[typeof Symbol.isConcatSpreadable]) return false;
+	// Return 'true' also for polyfills
 	if (!validTypes[typeof Symbol.iterator]) return false;
 	if (!validTypes[typeof Symbol.toPrimitive]) return false;
 	if (!validTypes[typeof Symbol.toStringTag]) return false;
-	if (!validTypes[typeof Symbol.unscopables]) return false;
 
 	return true;
 };
